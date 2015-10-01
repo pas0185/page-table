1dfd8ee7fb8cd1bfaba7e781766d1f61cde3bae6
410 <psheehan0301@gmail.com>
2015-09-30 21:30:44 -0500

    Configure register usage in PageTable for load and enable_paging

--- page_table.C
+++ page_table.C
@@ -12,7 +12,15 @@
 
 #include "page_table.H"
 
-  static void init_paging(FramePool * _kernel_mem_pool,
+
+  PageTable     * PageTable::current_page_table; /* pointer to currently loaded page table object */
+  unsigned int    PageTable::paging_enabled;     /* is paging turned on (i.e. are addresses logical)? */
+  FramePool     * PageTable::kernel_mem_pool;    /* Frame pool for the kernel memory */
+  FramePool     * PageTable::process_mem_pool;   /* Frame pool for the process memory */
+  unsigned long   PageTable::shared_size;        /* size of shared address space */
+
+
+  void PageTable::init_paging(FramePool * _kernel_mem_pool,
                           FramePool * _process_mem_pool,
                           const unsigned long _shared_size) {
 
@@ -28,21 +36,23 @@
 	PageTable::PageTable() {
 
     // Initialize the Page Directory (or Outer Page Table) with an address in the Kernel
-    page_directory = kernel_mem_pool->get_frame() * PAGE_SIZE;
+    page_directory = (unsigned long *) (kernel_mem_pool->get_frame() * PAGE_SIZE);
 
     // Set up the directory's first page_table which maps to the Kernel's 4MB
-    unsigned long * kernel_page_table = kernel_mem_pool->get_frame() * PAGE_SIZE;
+    unsigned long * kernel_page_table = (unsigned long *) (kernel_mem_pool->get_frame() * PAGE_SIZE);
 
-    unsigned long address = 0; // holds the physical address of where a page is
-    unsigned int i;
 
     // map the first 4MB of memory
+    unsigned long address = 0; // holds the physical address of where a page is
+    unsigned int i;
     for(i = 0; i < ENTRIES_PER_PAGE; i++) {
-      page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
+      kernel_page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
       address = address + PAGE_SIZE; // 4096 = 4kb
     };
 
-    page_directory[0] = page_table;
+    // Save put kernel page table in the directory and write it to register CR3
+    page_directory[0] = *kernel_page_table;
+    write_cr3(*kernel_page_table);
 
 	}
   /* Initializes a page table with a given location for the directory and the
@@ -55,19 +65,25 @@
 
   void PageTable::load() {
 
+    // Get the current page table from the CR3 register
+    current_page_table = read_cr3()
+
   }
   /* Makes the given page table the current table. This must be done once during
      system startup and whenever the address space is switched (e.g. during
      process switching). */
 
-  static void enable_paging() {
+  void PageTable::enable_paging() {
 
+    // SET BIT #31 IN THE CR0 REGISTER
+    unsigned long paging_bit = 0x1 << 31;
+    write_cr0(paging_bit);
   }
   /* Enable paging on the CPU. Typically, a CPU start with paging disabled, and
      memory is accessed by addressing physical memory directly. After paging is
      enabled, memory is addressed logically. */
 
-  static void handle_fault(REGS * _r) {
+  void PageTable::handle_fault(REGS * _r) {
 
   }
   /* The page fault handler. */
